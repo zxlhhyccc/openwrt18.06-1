709b2950d6a86d38eef3756ba13aef70f98ee32e
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2019-11-03 02:08:21 +0800

    Update servers-config.lua

--- package/my/luci-app-openclash/files/usr/lib/lua/luci/model/cbi/openclash/servers-config.lua
+++ package/my/luci-app-openclash/files/usr/lib/lua/luci/model/cbi/openclash/servers-config.lua
@@ -1,206 +1,188 @@
--- Copyright (C) 2017 yushi studio <ywb94@qq.com> github.com/ywb94
--- Licensed to the public under the GNU General Public License v3.
-
 local m, s, o
 local openclash = "openclash"
 local uci = luci.model.uci.cursor()
-local fs = require "nixio.fs"
-local sys = require "luci.sys"
-local sid = arg[1]
-local uuid = luci.sys.exec("cat /proc/sys/kernel/random/uuid")
 
-local encrypt_methods_ss = {
-	
-	-- stream
-	"rc4-md5",
-	"aes-128-cfb",
-	"aes-192-cfb",
-	"aes-256-cfb",
-	"aes-128-ctr",
-	"aes-192-ctr",
-	"aes-256-ctr",
-	"aes-128-gcm",
-	"aes-192-gcm",
-	"aes-256-gcm",
-	"chacha20",
-	"chacha20-ietf",
-	"xchacha20",
-	"chacha20-ietf-poly1305",
-	"xchacha20-ietf-poly1305",
-}
+font_red = [[<font color="red">]]
+font_off = [[</font>]]
+bold_on  = [[<strong>]]
+bold_off = [[</strong>]]
 
-local securitys = {
-    "auto",
-    "none",
-    "aes-128-gcm",
-    "chacha20-poly1305"
-}
-
-m = Map(openclash, translate("Edit Server"))
+m = Map(openclash,  translate("Servers manage and Config create"))
 m.pageaction = false
-m.redirect = luci.dispatcher.build_url("admin/services/openclash/servers")
 
-if m.uci:get(openclash, sid) ~= "servers" then
-	luci.http.redirect(m.redirect)
-	return
-end
-
--- [[ Servers Setting ]] --
-s = m:section(NamedSection, sid, "servers")
+s = m:section(TypedSection, "openclash")
 s.anonymous = true
-s.addremove   = false
 
-o = s:option(ListValue, "type", translate("Server Node Type"))
-o:value("ss", translate("Shadowsocks"))
-o:value("vmess", translate("Vmess"))
-o:value("socks5", translate("Socks5"))
-o:value("http", translate("HTTP(S)"))
+o = s:option(ListValue, "create_config", translate("Create Config"))
+o.description = font_red .. bold_on .. translate("Create Config By One-Click Only Need Proxys") .. bold_off .. font_off
+o:value("0", translate("Disable"))
+o:value("1", translate("Enable"))
+o.default=0
 
-o.description = translate("Using incorrect encryption mothod may causes service fail to start")
+o = s:option(ListValue, "rule_sources", translate("Choose Template For Create Config"))
+o.description = translate("Use Other Rules To Create Config")
+o:depends("create_config", 1)
+o:value("lhie1", translate("lhie1 Rules"))
+o:value("ConnersHua", translate("ConnersHua Rules"))
+o:value("ConnersHua_return", translate("ConnersHua Return Rules"))
 
-o = s:option(Value, "name", translate("Server Alias"))
-o.rmempty = false
+o = s:option(ListValue, "servers_update", translate("Keep Settings"))
+o.description = font_red .. bold_on .. translate("Only Update Servers Below When Subscription") .. bold_off .. font_off
+o:value("0", translate("Disable"))
+o:value("1", translate("Enable"))
+o.default=0
 
-o = s:option(Value, "server", translate("Server Address"))
-o.datatype = "host"
-o.rmempty = false
-
-o = s:option(Value, "port", translate("Server Port"))
-o.datatype = "port"
-o.rmempty = false
-
-o = s:option(Value, "password", translate("Password"))
-o.password = true
-o.rmempty = false
-o:depends("type", "ss")
-
-o = s:option(ListValue, "cipher", translate("Encrypt Method"))
-for _, v in ipairs(encrypt_methods_ss) do o:value(v) end
+o = s:option(DynamicList, "new_servers_group", translate("New Servers Group"))
+o.description = translate("Set The New Subscribe Server's Default Proxy Groups")
 o.rmempty = true
-o:depends("type", "ss")
-
-o = s:option(ListValue, "securitys", translate("Encrypt Method"))
-for _, v in ipairs(securitys) do o:value(v, v:upper()) end
-o.rmempty = true
-o:depends("type", "vmess")
-
-o = s:option(ListValue, "udp", translate("UDP Enable"))
-o.rmempty = true
-o.default = "false"
-o:value("true")
-o:value("false")
-o:depends("type", "ss")
-
-o = s:option(ListValue, "obfs", translate("obfs-mode"))
-o.rmempty = true
-o.default = "none"
-o:value("none")
-o:value("tls")
-o:value("http")
-o:value("websocket", translate("websocket (ws)"))
-o:depends("type", "ss")
-
-o = s:option(ListValue, "obfs_vmess", translate("obfs-mode"))
-o.rmempty = true
-o.default = "none"
-o:value("none")
-o:value("websocket", translate("websocket (ws)"))
-o:depends("type", "vmess")
-
-o = s:option(Value, "host", translate("obfs-hosts"))
-o.datatype = "host"
-o.rmempty = true
-o:depends("obfs", "tls")
-o:depends("obfs", "http")
-o:depends("obfs", "websocket")
-
-o = s:option(Value, "custom", translate("ws-headers"))
-o.rmempty = true
-o:depends("obfs", "websocket")
-o:depends("obfs_vmess", "websocket")
-
--- [[ WS部分 ]]--
-
--- WS路径
-o = s:option(Value, "path", translate("ws-path"))
-o.rmempty = true
-o:depends("obfs", "websocket")
-o:depends("obfs_vmess", "websocket")
-
--- AlterId
-o = s:option(Value, "alterId", translate("AlterId"))
-o.datatype = "port"
-o.default = 16
-o.rmempty = true
-o:depends("type", "vmess")
-
--- VmessId
-o = s:option(Value, "uuid", translate("VmessId (UUID)"))
-o.rmempty = true
-o.default = uuid
-o:depends("type", "vmess")
-
--- 验证用户名
-o = s:option(Value, "auth_name", translate("Auth Username"))
-o:depends("type", "socks5")
-o:depends("type", "http")
-o.rmempty = true
-
--- 验证密码
-o = s:option(Value, "auth_pass", translate("Auth Password"))
-o:depends("type", "socks5")
-o:depends("type", "http")
-o.rmempty = true
-
--- [[ skip-cert-verify ]]--
-o = s:option(ListValue, "skip_cert_verify", translate("Skip Cert Verify"))
-o.rmempty = true
-o.default = "false"
-o:value("true")
-o:value("false")
-o:depends("obfs", "websocket")
-o:depends("obfs_vmess", "websocket")
-o:depends("type", "socks5")
-o:depends("type", "http")
-
--- [[ TLS ]]--
-o = s:option(ListValue, "tls", translate("TLS"))
-o.rmempty = true
-o.default = "false"
-o:value("true")
-o:value("false")
-o:depends("obfs", "websocket")
-o:depends("obfs_vmess", "websocket")
-o:depends("type", "socks5")
-o:depends("type", "http")
-
-o = s:option(DynamicList, "groups", translate("Proxy Group"))
-o.description = translate("No Need Set when Config Create, The added Proxy Groups Must Exist")
-o.rmempty = true
-uci:foreach("openclash", "groups",
+o:depends("servers_update", 1)
+m.uci:foreach("openclash", "groups",
 		function(s)
 			o:value(s.name)
 		end)
 
-local t = {
-    {Commit, Back}
+-- [[ Groups Manage ]]--
+s = m:section(TypedSection, "groups", translate("Proxy Groups(No Need Set when Config Create)"))
+s.anonymous = true
+s.addremove = true
+s.sortable = true
+s.template = "cbi/tblsection"
+s.extedit = luci.dispatcher.build_url("admin/services/openclash/groups-config/%s")
+function s.create(...)
+	local sid = TypedSection.create(...)
+	if sid then
+		luci.http.redirect(s.extedit % sid)
+		return
+	end
+end
+
+o = s:option(DummyValue, "type", translate("Group Type"))
+function o.cfgvalue(...)
+	return Value.cfgvalue(...) or translate("None")
+end
+
+o = s:option(DummyValue, "name", translate("Group Name"))
+function o.cfgvalue(...)
+	return Value.cfgvalue(...) or translate("None")
+end
+
+-- [[ Servers Manage ]]--
+s = m:section(TypedSection, "servers", translate("Proxys"))
+s.anonymous = true
+s.addremove = true
+s.sortable = true
+s.template = "cbi/tblsection"
+s.extedit = luci.dispatcher.build_url("admin/services/openclash/servers-config/%s")
+function s.create(...)
+	local sid = TypedSection.create(...)
+	if sid then
+		luci.http.redirect(s.extedit % sid)
+		return
+	end
+end
+
+---- enable flag
+o = s:option(Flag, "enabled", translate("Enable"))
+o.rmempty     = false
+o.default     = o.enabled
+o.cfgvalue    = function(...)
+    return Flag.cfgvalue(...) or "1"
+end
+
+o = s:option(DummyValue, "type", translate("Type"))
+function o.cfgvalue(...)
+	return Value.cfgvalue(...) or translate("None")
+end
+
+o = s:option(DummyValue, "name", translate("Server Alias"))
+function o.cfgvalue(...)
+	return Value.cfgvalue(...) or translate("None")
+end
+
+o = s:option(DummyValue, "server", translate("Server Address"))
+function o.cfgvalue(...)
+	return Value.cfgvalue(...) or translate("None")
+end
+
+o = s:option(DummyValue, "port", translate("Server Port"))
+function o.cfgvalue(...)
+	return Value.cfgvalue(...) or translate("None")
+end
+
+o = s:option(DummyValue,"server",translate("Ping Latency"))
+o.template="openclash/ping"
+o.width="10%"
+
+local tt = {
+    {Delete_Unused_Servers, Delete_Severs, Delete_Groups}
 }
+
+b = m:section(Table, tt)
+
+o = b:option(Button,"Delete_Unused_Servers")
+o.inputtitle = translate("Delete Unused Servers")
+o.inputstyle = "reset"
+o.write = function()
+  m.uci:set("openclash", "config", "enable", 0)
+  m.uci:commit("openclash")
+  luci.sys.call("sh /usr/share/openclash/cfg_unused_servers_del.sh 2>/dev/null")
+  luci.http.redirect(luci.dispatcher.build_url("admin", "services", "openclash", "servers"))
+end
+
+o = b:option(Button,"Delete_Severs")
+o.inputtitle = translate("Delete Severs")
+o.inputstyle = "reset"
+o.write = function()
+  m.uci:set("openclash", "config", "enable", 0)
+  m.uci:delete_all("openclash", "servers", function(s) return true end)
+  m.uci:commit("openclash")
+  luci.http.redirect(luci.dispatcher.build_url("admin", "services", "openclash", "servers"))
+end
+
+o = b:option(Button,"Delete_Groups")
+o.inputtitle = translate("Delete Groups")
+o.inputstyle = "reset"
+o.write = function()
+  m.uci:set("openclash", "config", "enable", 0)
+  m.uci:delete_all("openclash", "groups", function(s) return true end)
+  m.uci:commit("openclash")
+  luci.http.redirect(luci.dispatcher.build_url("admin", "services", "openclash", "servers"))
+end
+
+local t = {
+    {Load_Config, Commit, Apply}
+}
+
 a = m:section(Table, t)
 
-o = a:option(Button,"Commit")
+o = a:option(Button,"Load_Config")
+o.inputtitle = translate("Load Config")
+o.inputstyle = "apply"
+o.write = function()
+  m.uci:set("openclash", "config", "enable", 0)
+  m.uci:commit("openclash")
+  luci.sys.call("sh /usr/share/openclash/yml_groups_get.sh 2>/dev/null &")
+  luci.http.redirect(luci.dispatcher.build_url("admin", "services", "openclash"))
+end
+
+o = a:option(Button, "Commit") 
 o.inputtitle = translate("Commit Configurations")
 o.inputstyle = "apply"
 o.write = function()
-   uci:commit(openclash, sid)
-   luci.http.redirect(luci.dispatcher.build_url("admin", "services", "openclash", "servers"))
+  m.uci:set("openclash", "config", "enable", 0)
+  m.uci:commit("openclash")
 end
 
-o = a:option(Button,"Back")
-o.inputtitle = translate("Back Configurations")
-o.inputstyle = "reset"
+o = a:option(Button, "Apply")
+o.inputtitle = translate("Apply Configurations")
+o.inputstyle = "apply"
 o.write = function()
-   uci:revert(openclash, sid)
-   luci.http.redirect(luci.dispatcher.build_url("admin", "services", "openclash", "servers"))
+  m.uci:set("openclash", "config", "enable", 0)
+  m.uci:commit("openclash")
+  luci.sys.call("/usr/share/openclash/yml_groups_set.sh >/dev/null 2>&1 &")
+  luci.http.redirect(luci.dispatcher.build_url("admin", "services", "openclash"))
 end
 
+m:append(Template("openclash/server_list"))
 return m
+
