a6ab1e63e0f4e0729d642c95aec76b44db2e747f
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2019-12-10 22:08:28 +0800

    Update subscription.lua

--- package/lean/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/subscription.lua
+++ package/lean/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/subscription.lua
@@ -9,19 +9,18 @@ uci:foreach("shadowsocksr", "servers", function(s)
   server_count = server_count + 1
 end)
 
+local fs  = require "nixio.fs"
+local sys = require "luci.sys"
 m = Map(shadowsocksr)
 m:section(SimpleSection).template  = "shadowsocksr/status"
--- Server Subscribe
 
-s = m:section(TypedSection, "server_subscribe",  translate("Servers subscription and manage"))
+
+s = m:section(TypedSection, "server_subscribe", translate("Node Subscribe"))
 s.anonymous = true
-
 
 o = s:option(Flag, "auto_update", translate("Auto Update"))
 o.rmempty = false
 o.description = translate("Auto Update Server subscription, GFW list and CHN route")
-
-
 
 o = s:option(ListValue, "edition", translate("V2 Edition"))
 o:value(" 4.21.3 ", " 4.21.3 ")
@@ -31,8 +30,6 @@ o:value("4.18.2", "4.18.2")
 o:value("4.18.1", "4.18.1")
 o:value("4.18.0", "4.18.0")
 o.rmempty = false
-
-
 
 o = s:option(ListValue, "auto_update_time", translate("Update time (every day)"))
 for t = 0,23 do
@@ -48,9 +45,8 @@ o = s:option(Button,"update_Sub",translate("Update Subscribe List"))
 o.inputstyle = "reload"
 o.description = translate("Update subscribe url list first")
 o.write = function() 
-  luci.http.redirect(luci.dispatcher.build_url("admin", "services", "shadowsocksr", "servers"))
+  luci.http.redirect(luci.dispatcher.build_url("admin", "vpn", "shadowsocksr", "servers"))
 end
-
 o = s:option(Flag, "proxy", translate("Through proxy update"))
 o.rmempty = false
 o.description = translate("Through proxy update list, Not Recommended ")
@@ -59,12 +55,8 @@ o = s:option(Button,"update",translate("Update All Subscribe Severs"))
 o.inputstyle = "apply"
 o.write = function() 
   luci.sys.exec("bash /usr/share/shadowsocksr/subscribe.sh >>/tmp/ssrplus.log 2>&1")
-  luci.http.redirect(luci.dispatcher.build_url("admin", "services", "shadowsocksr", "servers"))
+  luci.http.redirect(luci.dispatcher.build_url("admin", "vpn", "shadowsocksr", "servers"))
 end
-
-o = s:option(DummyValue, "", "")
-o.rawhtml = true
-o.template = "shadowsocksr/update_subscribe"
 
 o = s:option(Button,"update_v2ray",translate("Upgrade V2ray"))
 o.inputstyle = "reload"
@@ -72,19 +64,16 @@ o.write = function()
   luci.sys.call("bash /usr/share/shadowsocksr/v2ray_update1.sh >>/tmp/ssrplus.log 2>&1")
 end
 
-
-
-
 o = s:option(Button,"delete",translate("Delete all severs"))
 o.inputstyle = "reset"
 o.description = string.format(translate("Server Count") ..  ": %d", server_count)
 o.write = function()
-    uci:delete_all("shadowsocksr", "servers", function(s) return true end)
-    luci.sys.call("uci commit shadowsocksr && /etc/init.d/shadowsocksr stop")
-    luci.http.redirect(luci.dispatcher.build_url("admin", "services", "shadowsocksr", "subscription"))
+  uci:delete_all("shadowsocksr", "servers", function(s) return true end)
+  uci:save("shadowsocksr") 
+  luci.sys.call("uci commit shadowsocksr && /etc/init.d/shadowsocksr stop") 
+  luci.http.redirect(luci.dispatcher.build_url("admin", "vpn", "shadowsocksr", "servers"))
+  return
 end
 
-
-
-
 return m
+
